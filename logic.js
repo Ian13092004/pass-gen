524a2a179350c28c6c72b773647b41e9149d97bc
Erick Bonruque <83654537+ErickBonruque@users.noreply.github.com>
2024-06-12 21:46:01 -0300

    Sexo

---
+++ logic.js
@@ -0,0 +1,13 @@
+function updateValue(val) {
+    document.getElementById('sliderValue').textContent = val;
+}
+
+function GeradorDeSenha
+{
+    
+}
+
+const LetraMinuscula;
+const LetraMaiucula;
+const Numero;
+const simbolo;
